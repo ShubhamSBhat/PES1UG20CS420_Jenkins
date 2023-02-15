07699ea9d663b964390b77b0cc1c81b895c903d9
ShubhamSBhat <shubham.s.bhat@gmail.com>
2023-02-15 13:52:44 +0530

    First Commit

---
+++ main/hello.cpp
@@ -0,0 +1,12 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run\n";
+  cout << "This is SRN: PES1UG20CS420 \n";
+  cout << "This is Name: Shubham S \n";
+  return 0;
+}
